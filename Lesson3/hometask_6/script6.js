34ab803f90f5e506849f134d02242a017c00bc07
Olga-Vretto <olgavretto@outlook.com>
2023-01-19 00:37:25 +0200

    script6.js

---
+++ Lesson3/hometask_6/script6.js
@@ -0,0 +1,11 @@
+const strOne = prompt('Please write the first string:', '');
+const strTwo = prompt('Please write the second string:', '');
+const strThree = prompt('Please write the third string:', '');
+
+alert(`${strOne + strTwo + strThree}`);
+
+
+const number = +prompt('Write five-digit number:','12345');
+const myString = number.toString();
+
+alert (`${myString[0]} ${myString[1]} ${myString[2]} ${myString[3]} ${myString[4]}`);
