13d5e9f30d82b497d4aff960460a286e0d0d6a05
Olga-Vretto <olgavretto@outlook.com>
2023-01-20 13:36:48 +0200

    finished task8

--- Lesson3/hometask_8/script8.js
+++ Lesson3/hometask_8/script8.js
@@ -1,16 +1,19 @@
-const currentYear = Number(new Date().getFullYear());
-let age = currentYear - Number(prompt("Вкажіть рік народження:", "2000"));
-const city = prompt("Вкажіть місто проживання:", "Київ");
-sport = prompt("Вкажіть улюблений вид спорту", "Бокс");
+const age = prompt("Вкажіть рік народження:", "");
+const city = prompt("Вкажіть місто проживання:", "");
+const sport = prompt("Вкажіть улюблений вид спорту", "");
 
 let messageAge;
 let messageCity;
 let messageSport;
 
-if (age === null || age.trim === "") {
-  messageAge = "Шкода, що Ви не захотіли ввести свій рік народження.";
-} else {
-  messageAge = `Тобі ${age}.`;
+switch (age) {
+  case "":
+  case null:
+    messageAge = "Шкода, що Ви не захотіли ввести свій рік народження.";
+    break;
+  default:
+    messageAge = `Тобі ${2023 - age}.`;
+    break;
 }
 
 switch (city) {
@@ -26,6 +29,7 @@ switch (city) {
   case "":
   case null:
     messageCity = "Шкода, що Ви не захотіли ввести своє місто.";
+    break;
   default:
     messageCity = `Ти живеш у місті ${city}.`;
     break;
@@ -43,10 +47,11 @@ switch (sport) {
     break;
   case "":
   case null:
-    messageSport = `Шкода, що Ви не захотіли ввести свій улюблений вид спорту.`;
+    messageSport = "Шкода, що Ви не захотіли ввести свій улюблений вид спорту.";
+    break;
   default:
     messageSport = `Твій улюблений вид спорту: ${sport}.`;
     break;
 }
 
-alert(`${messageAge} ${messageCity} ${messageSport}`);
+alert(`${messageAge} \n ${messageCity} \n ${messageSport}`);
