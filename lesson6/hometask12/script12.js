a17be4e1121eb3bff22a4e8e14785de719b1267a
Olga-Vretto <olgavretto@outlook.com>
2023-01-27 01:12:06 +0200

    add hw12 and hw15

---
+++ lesson6/hometask12/script12.js
@@ -0,0 +1,10 @@
+const numbersArray = [ 3, 6, 1, 5, 4, 2];
+console.log(numbersArray);
+
+numbersArray.sort( (a, b) => a - b );
+console.log(numbersArray);
+numbersArray.splice(1, 3);
+console.log(numbersArray);
+
+
+    
---
+++ lesson7/homework_15/script15.js
@@ -0,0 +1,6 @@
+const fruitsArray = ["apple", "banana", "orange", "pear"];
+
+fruitsArray.forEach((element) => console.log(element));
+for (const fruit of fruitsArray) {
+  console.log(fruit);
+}
