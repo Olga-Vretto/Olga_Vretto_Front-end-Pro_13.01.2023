f79a06ebee7b03872bc8b0c88a025e56b195a04f
Olga-Vretto <olgavretto@outlook.com>
2023-02-03 01:13:54 +0200

    changed task 17

--- lesson9/homework_17/script17.js
+++ lesson9/homework_17/script17.js
@@ -2,8 +2,9 @@ const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
 function generateKey(length, characters) {
   let key = "";
-  for (let i = 0; i < length; i++)
+  for (let i = 0; i < length; i++) {
     key += characters.charAt(Math.random() * characters.length);
+  }
   return key;
 }
 console.log(generateKey(16, characters));
--- lesson9/homework_18/script18.js
+++ lesson9/homework_18/script18.js
@@ -1,59 +1,64 @@
-//  Task 1
+// //  Task 1
 
-const array = [1, "apple", 8, "true", 14, 20, { name: "Bob" }, 5];
-const numbersFromArr = array.filter((element) => typeof element === "number");
+// const array = [1, "apple", 8, "true", 14, 20, { name: "Bob" }, 5];
+// const numbersFromArr = array.filter((element) => typeof element === "number");
 
-function average(arr) {
-  let arithmeticMean = 0;
-  for (let i = 0; i < arr.length; i++) {
-    arithmeticMean += arr[i];
-  }
-  return arithmeticMean / arr.length;
-}
-console.log(average(numbersFromArr));
+// function average(arr) {
+//   let arithmeticMean = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     arithmeticMean += arr[i];
+//   }
+//   return arithmeticMean / arr.length;
+// }
+// console.log(average(numbersFromArr));
 
 
-//  Task 2
+// //  Task 2
 
-const number1 = +prompt("Введіть перше число",'');
-const mathSign = prompt("Введіть знак",'');
-const number2 = +prompt("Введіть друге число",'');
+// const number1 = +prompt("Введіть перше число",'');
+// const mathSign = prompt("Введіть знак",'');
+// const number2 = +prompt("Введіть друге число",'');
 
-function doMath(x, znak, y) {
-  let result = 0;
+// function doMath(x, znak, y) {
+//   let result = 0;
 
-  switch (znak) {
-    case "+":
-      result = x + y;
-      break;
-    case "-":
-      result = x - y;
-      break;
-    case "*":
-      result = x * y;
-      break;
-    case "/":
-      result = x / y;
-      break;
-    case "%":
-      result = (x * y) / 100;
-      break;
-    case "^":
-      result = x ** y;
-      break;
-    default:
-      result = "Спробуйте ще.";
-      break;
-  }
+//   switch (znak) {
+//     case "+":
+//       result = x + y;
+//       break;
+//     case "-":
+//       result = x - y;
+//       break;
+//     case "*":
+//       result = x * y;
+//       break;
+//     case "/":
+//       result = x / y;
+//       break;
+//     case "%":
+//       result = (x * y) / 100;
+//       break;
+//     case "^":
+//       result = x ** y;
+//       break;
+//     default:
+//       result = "Спробуйте ще.";
+//       break;
+//   }
 
-  return result;
-}
-console.log(doMath(number1, mathSign, number2));
+//   return result;
+// }
+// console.log(doMath(number1, mathSign, number2));
 
 
 //  Task 3
 
-const arrOneLength = Number(prompt('Введіть довжину першого масиву:',''));
-const arrTwoLenght = Number(prompt('Введіть довжину другого масиву:',''));
+// const arrOneLength = Number(prompt('Введіть довжину першого масиву:',''));
+// const arrTwoLength = Number(prompt('Введіть довжину другого масиву:',''));
 
-
+// function fillArray(arrOneLength, arrTwoLength) {
+//   for (i = 0; i < arrOneLength; i++) {
+//     arrOneLength = arrTwoLength
+//   }
+//   return arrOneLength
+// }
