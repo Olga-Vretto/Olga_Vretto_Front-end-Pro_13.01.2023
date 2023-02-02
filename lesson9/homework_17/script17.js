6d1239e64c7a720ca5b53c661a52f1757fbf6520
Olga-Vretto <olgavretto@outlook.com>
2023-02-03 00:28:00 +0200

    changed task17

--- lesson9/homework_17/script17.js
+++ lesson9/homework_17/script17.js
@@ -2,11 +2,8 @@ const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
 function generateKey(length, characters) {
   let key = "";
-  let countNum = 0;
-  while (countNum < length) {
+  for (let i = 0; i < length; i++)
     key += characters.charAt(Math.random() * characters.length);
-    countNum += 1;
-  }
   return key;
 }
 console.log(generateKey(16, characters));
