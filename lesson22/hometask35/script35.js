9abd2789c0f85636f015c73d6cfc96a9430d0bab
Olga-Vretto <olgavretto@outlook.com>
2023-03-24 13:26:26 +0200

    changed task35

--- lesson22/hometask35/script35.js
+++ lesson22/hometask35/script35.js
@@ -4,7 +4,7 @@ for (let i = 0; i < 10; i++) {
   const tableRow = document.createElement("tr");
   for (let j = 0; j < 10; j++) {
     const tableData = document.createElement("td");
-    tableData.innerHTML = i * 10 + j + 1;
+    tableData.innerHTML = Math.floor(Math.random() * 100) + 1;
     tableData.style.padding = "10px";
     tableRow.append(tableData);
   }
