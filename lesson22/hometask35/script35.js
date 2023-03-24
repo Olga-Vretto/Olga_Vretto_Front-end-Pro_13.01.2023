1c0361062aee33866fd44b0c7b9c6c3cbd31f4ae
Olga-Vretto <olgavretto@outlook.com>
2023-03-24 13:16:10 +0200

    adding task 35

---
+++ lesson22/hometask35/script35.js
@@ -0,0 +1,13 @@
+const body = document.getElementById("my-table");
+const table = document.createElement("table");
+for (let i = 0; i < 10; i++) {
+  const tableRow = document.createElement("tr");
+  for (let j = 0; j < 10; j++) {
+    const tableData = document.createElement("td");
+    tableData.innerHTML = i * 10 + j + 1;
+    tableData.style.padding = "10px";
+    tableRow.append(tableData);
+  }
+  table.append(tableRow);
+}
+body.append(table);
