27916edf5b8ec3dd2361843a9ea491203e017262
Olga-Vretto <olgavretto@outlook.com>
2023-03-27 23:42:49 +0300

    task38

---
+++ lesson23/hometask_38/script38.js
@@ -0,0 +1,39 @@
+const images = [
+  "./assets/images2.jpg",
+  "./assets/images/3.jpg",
+  "./assets/images/4.jpg",
+  "./assets/images/5.jpg",
+  "./assets/images/6.jpg",
+];
+
+const slider = document.getElementById("slider");
+const image = document.getElementById("image");
+const buttonPrev = document.getElementById("btn-prev");
+const buttonNext = document.getElementById("btn-next");
+let countImage = 0;
+
+if (countImage === 0) {
+  buttonPrev.style.display = "none";
+}
+
+buttonPrev.addEventListener("click", () => {
+  countImage--;
+  image.src = images[countImage];
+  if (countImage === 0) {
+    buttonPrev.style.display = "none";
+  }
+  if (countImage < images.length - 1) {
+    buttonNext.style.display = "block";
+  }
+});
+
+buttonNext.addEventListener("click", () => {
+  countImage++;
+  image.src = images[countImage];
+  if (countImage === images.length - 1) {
+    buttonNext.style.display = "none";
+  }
+  if (countImage > 0) {
+    buttonPrev.style.display = "block";
+  }
+});
